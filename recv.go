654ad4c14a621e35e79b5807bd4adebd24173d8f
bemasher <bemasher@gmail.com>
2014-12-01 23:19:29 -0700

    Pattern retracing complete.

--- recv.go
+++ recv.go
@@ -43,11 +43,19 @@ var rcvr Receiver
 
 type Receiver struct {
 	rtltcp.SDR
-	d        decode.Decoder
-	p        parse.Parser
-	pIdx     int
-	pattern  []int
+	d decode.Decoder
+	p parse.Parser
+
+	chIdx    int
 	channels []uint32
+
+	last    time.Time
+	pIdx    int
+	pattern map[int]map[int]int
+
+	centers     []int
+	centerOrder [50][]int
+	centerIdx   [50]int
 }
 
 func (rcvr *Receiver) NewReceiver() {
@@ -63,19 +71,25 @@ func (rcvr *Receiver) NewReceiver() {
 	}
 
 	rcvr.channels = []uint32{
-		909586111, 909782679, 909979247, 910175815, 911224178, 911420746,
-		911617314, 911813882, 912010451, 912207019, 912403587, 912600155,
-		912796723, 912993291, 913189859, 913386427, 913582995, 913779563,
-		913976132, 915024495, 915221063, 915417631, 915614199, 915810767,
-		916007335, 916203903, 916400471, 916597040, 916793608, 916990176,
-		917186744, 917383312, 917579880, 917776448, 918824811, 919021379,
-		919217947, 919414516, 919611084, 919807652, 920004220, 920200788,
-		920397356, 920593924, 920790492, 920987060, 921183628, 921380197,
-		921576765, 921773333,
+		909586111, 909782679, 909979247, 910175815, 911224178, 911420746, // 0
+		911617314, 911813882, 912010451, 912207019, 912403587, 912600155, // 6
+		912796723, 912993291, 913189859, 913386427, 913582995, 913779563, // 12
+		913976132, 915024495, 915221063, 915417631, 915614199, 915810767, // 18
+		916007335, 916203903, 916400471, 916597040, 916793608, 916990176, // 24
+		917186744, 917383312, 917579880, 917776448, 918824811, 919021379, // 30
+		919217947, 919414516, 919611084, 919807652, 920004220, 920200788, // 36
+		920397356, 920593924, 920790492, 920987060, 921183628, 921380197, // 42
+		921576765, 921773333, // 44
 	}
-	rcvr.pIdx = 0
-	rcvr.pattern = rand.Perm(50)
-	log.Println("Pattern:", rcvr.pattern)
+
+	rcvr.chIdx = 42
+	rcvr.pattern = make(map[int]map[int]int)
+
+	rcvr.centers = []int{0, 9, 13, 23, 28, 39, 44}
+	for idx := range rcvr.centerIdx {
+		rcvr.centerIdx[idx] = 0
+		rcvr.centerOrder[idx] = rand.Perm(len(rcvr.centers))
+	}
 
 	if !*quiet {
 		rcvr.d.Cfg.Log()
@@ -94,35 +108,11 @@ func (rcvr *Receiver) NewReceiver() {
 		log.Println("GainCount:", rcvr.SDR.Info.GainCount)
 	}
 
-	centerfreqFlagSet := false
-	sampleRateFlagSet := false
-	gainFlagSet := false
-	flag.Visit(func(f *flag.Flag) {
-		switch f.Name {
-		case "centerfreq":
-			centerfreqFlagSet = true
-		case "samplerate":
-			sampleRateFlagSet = true
-		case "gainbyindex", "tunergainmode", "tunergain", "agcmode":
-			gainFlagSet = true
-		}
-	})
-
-	// Set some parameters for listening.
-	if *hop {
-		rcvr.SetCenterFreq(rcvr.channels[rcvr.pattern[rcvr.pIdx]])
-	} else {
-		if !centerfreqFlagSet {
-			rcvr.SetCenterFreq(uint32(rcvr.Flags.CenterFreq))
-		}
-	}
-
-	if !sampleRateFlagSet {
-		rcvr.SetSampleRate(uint32(rcvr.d.Cfg.SampleRate))
-	}
-	if !gainFlagSet {
-		rcvr.SetGainMode(true)
-	}
+	rcvr.SetSampleRate(uint32(rcvr.d.Cfg.SampleRate))
+	rcvr.SetCenterFreq(rcvr.channels[rcvr.chIdx])
+	rcvr.SetAGCMode(false)
+	rcvr.SetGainMode(false)
+	rcvr.SetGainByIndex(5)
 
 	return
 }
@@ -140,6 +130,9 @@ func (rcvr *Receiver) Run() {
 
 	block := make([]byte, rcvr.d.Cfg.BlockSize2)
 
+	isOffset := false
+	var interval time.Duration
+
 	start := time.Now()
 	for {
 		// Exit on interrupt or time limit, otherwise receive.
@@ -156,41 +149,54 @@ func (rcvr *Receiver) Run() {
 				log.Fatal("Error reading samples: ", err)
 			}
 
+			if isOffset {
+				interval = 30 * time.Second
+			} else {
+				interval = 45 * time.Second
+			}
+
+			if !rcvr.last.IsZero() && time.Since(rcvr.last) > interval {
+				isOffset = true
+
+				// Missing on a retrace is a penalty to the channel retraced.
+				for ch := range rcvr.pattern[rcvr.pIdx] {
+					if ch == rcvr.chIdx {
+						rcvr.pattern[rcvr.pIdx][ch]--
+					} else {
+						rcvr.pattern[rcvr.pIdx][ch]++
+					}
+				}
+
+				log.Println("Missed: ", rcvr.pIdx, rcvr.chIdx)
+
+				rcvr.NextChannel()
+			}
+
 			pktFound := false
 			for _, pkt := range rcvr.d.Decode(block) {
-				scm, err := rcvr.p.Parse(parse.NewDataFromBytes(pkt))
+				rawMsg, err := rcvr.p.Parse(parse.NewDataFromBytes(pkt))
 				if err != nil {
 					// log.Println(err)
 					continue
 				}
 
-				if len(meterID) > 0 && !meterID[uint(scm.MeterID())] {
+				if len(meterID) > 0 && !meterID[uint(rawMsg.MeterID())] {
 					continue
 				}
 
-				if len(meterType) > 0 && !meterType[uint(scm.MeterType())] {
+				if len(meterType) > 0 && !meterType[uint(rawMsg.MeterType())] {
 					continue
 				}
+
+				channel := rcvr.d.Periodogram.Execute(rcvr.d.Re, rcvr.d.Im)
 
 				var msg parse.Logger
-				if *hop {
-					msg = parse.HopMessage{
-						time.Now(),
-						scm.MeterID(),
-						scm.MeterType(),
-						rcvr.pattern[rcvr.pIdx],
-						rcvr.d.Periodogram.Execute(rcvr.d.Re, rcvr.d.Im),
-					}
-				} else {
-					offset, _ := sampleFile.Seek(0, os.SEEK_CUR)
-
-					msg = parse.LogMessage{
-						time.Now(),
-						offset,
-						rcvr.d.Cfg.BufferLength << 1,
-						rcvr.pattern[rcvr.pIdx] + rcvr.d.Periodogram.Execute(rcvr.d.Re, rcvr.d.Im),
-						scm,
-					}
+				msg = parse.HopMessage{
+					time.Now(),
+					rawMsg.MeterID(),
+					rawMsg.MeterType(),
+					rcvr.chIdx,
+					channel,
 				}
 
 				if encoder == nil {
@@ -215,18 +221,28 @@ func (rcvr *Receiver) Run() {
 
 				pktFound = true
 
-				if *hop && scm.MeterID() == 17581447 {
-					rcvr.pIdx++
-
-					if rcvr.pIdx == 50 {
-						rcvr.pattern = rand.Perm(50)
-						rcvr.pIdx = 0
-						log.Println("Pattern:", rcvr.pattern)
-					}
-
-					rcvr.SetCenterFreq(rcvr.channels[rcvr.pattern[rcvr.pIdx]])
-					rcvr.d.Reset()
+				if _, exists := rcvr.pattern[rcvr.pIdx]; !exists {
+					rcvr.pattern[rcvr.pIdx] = make(map[int]int)
 				}
+
+				// If this isn't a retrace, add it to the pattern.
+				if _, exists := rcvr.pattern[rcvr.pIdx][rcvr.chIdx+channel]; !exists {
+					rcvr.pattern[rcvr.pIdx][rcvr.chIdx+channel]++
+				} else {
+					// Reward retrace, penalize others.
+					for ch := range rcvr.pattern[rcvr.pIdx] {
+						if ch == rcvr.chIdx+channel {
+							rcvr.pattern[rcvr.pIdx][ch]++
+						} else {
+							rcvr.pattern[rcvr.pIdx][ch]--
+						}
+					}
+				}
+
+				log.Printf("%#v\n", rcvr.pattern)
+
+				isOffset = false
+				rcvr.NextChannel()
 
 				if *single {
 					break
@@ -247,6 +263,59 @@ func (rcvr *Receiver) Run() {
 			}
 		}
 	}
+}
+
+func (rcvr *Receiver) NextChannel() {
+	defer rcvr.d.Reset()
+
+	rcvr.last = time.Now()
+	rcvr.centerIdx[rcvr.pIdx] = (rcvr.centerIdx[rcvr.pIdx] + 1) % len(rcvr.centers)
+	rcvr.pIdx = (rcvr.pIdx + 1) % 50
+
+	// Prune bad channels.
+	for idx := range rcvr.pattern {
+		for ch, count := range rcvr.pattern[idx] {
+			if count <= 0 || ch < 0 || ch >= 50 {
+				delete(rcvr.pattern[idx], ch)
+			}
+		}
+	}
+
+	pattern := make([]int, 50)
+	for idx := range pattern {
+		pattern[idx] = -1
+	}
+	for idx, channels := range rcvr.pattern {
+		max := ^int(^uint(0) >> 1)
+		for ch, count := range channels {
+			if max < count {
+				max = count
+				pattern[idx] = ch
+			}
+		}
+	}
+	encoder.Encode(struct {
+		Time    time.Time
+		Pattern []int
+	}{time.Now(), pattern})
+
+	rcvr.chIdx = rcvr.centers[rcvr.centerOrder[rcvr.pIdx][rcvr.centerIdx[rcvr.pIdx]]]
+
+	max := ^int(^uint(0) >> 1)
+	for ch, count := range rcvr.pattern[rcvr.pIdx] {
+		if max < count {
+			max = count
+			rcvr.chIdx = ch
+		}
+	}
+
+	if max != ^int(^uint(0)>>1) {
+		log.Println("Retrace:", rcvr.pIdx, rcvr.chIdx)
+	} else {
+		log.Println("Guess:  ", rcvr.pIdx, rcvr.chIdx)
+	}
+
+	rcvr.SetCenterFreq(rcvr.channels[rcvr.chIdx])
 }
 
 func init() {
