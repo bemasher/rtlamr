8a570305da8a97e17d579faad5f6188cecc4a1fd
bemasher <bemasher@gmail.com>
2015-01-22 02:12:06 -0700

    Add r900 decoder.

---
+++ r900/gf/gf.go
@@ -0,0 +1,172 @@
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package gf implements arithmetic over Galois Fields. Generalized for any valid order.
+package gf
+
+import "strconv"
+
+// A Field represents an instance of GF(order) defined by a specific polynomial.
+type Field struct {
+	order int
+	log   []byte // log[0] is unused
+	exp   []byte
+}
+
+// NewField returns a new field corresponding to the polynomial poly and
+// generator α. The choice of generator α only affects the Exp and Log
+// operations.
+func NewField(order, poly, α int) *Field {
+	if order < 0 || order > 256 {
+		panic("gf: invalid order: " + strconv.Itoa(order))
+	}
+
+	if poly < order || poly >= order<<1 || reducible(poly) {
+		panic("gf: invalid polynomial: " + strconv.Itoa(poly))
+	}
+
+	f := Field{order - 1, make([]byte, order), make([]byte, (order-1)<<1)}
+	x := 1
+	for i := 0; i < f.order; i++ {
+		if x == 1 && i != 0 {
+			panic("gf: invalid generator " + strconv.Itoa(α) +
+				" for polynomial " + strconv.Itoa(poly))
+		}
+		f.exp[i] = byte(x)
+		f.exp[i+f.order] = byte(x)
+		f.log[x] = byte(i)
+		x = mul(x, α, order, poly)
+	}
+	f.log[0] = byte(f.order)
+	for i := 0; i < f.order; i++ {
+		if f.log[f.exp[i]] != byte(i) {
+			panic("bad log")
+		}
+		if f.log[f.exp[i+f.order]] != byte(i) {
+			panic("bad log")
+		}
+	}
+	for i := 1; i < order; i++ {
+		if f.exp[f.log[i]] != byte(i) {
+			panic("bad log")
+		}
+	}
+
+	return &f
+}
+
+// nbit returns the number of significant in p.
+func nbit(p int) uint {
+	n := uint(0)
+	for ; p > 0; p >>= 1 {
+		n++
+	}
+	return n
+}
+
+// polyDiv divides the polynomial p by q and returns the remainder.
+func polyDiv(p, q int) int {
+	np := nbit(p)
+	nq := nbit(q)
+	for ; np >= nq; np-- {
+		if p&(1<<(np-1)) != 0 {
+			p ^= q << (np - nq)
+		}
+	}
+	return p
+}
+
+// mul returns the product x*y mod poly, a GF(order) multiplication.
+func mul(x, y, order, poly int) int {
+	z := 0
+	for x > 0 {
+		if x&1 != 0 {
+			z ^= y
+		}
+		x >>= 1
+		y <<= 1
+		if y&order != 0 {
+			y ^= poly
+		}
+	}
+	return z
+}
+
+// reducible reports whether p is reducible.
+func reducible(p int) bool {
+	// Multiplying n-bit * n-bit produces (2n-1)-bit,
+	// so if p is reducible, one of its factors must be
+	// of np/2+1 bits or fewer.
+	np := nbit(p)
+	for q := 2; q < 1<<(np/2+1); q++ {
+		if polyDiv(p, q) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// Add returns the sum of x and y in the field.
+func (f *Field) Add(x, y byte) byte {
+	return x ^ y
+}
+
+// Exp returns the base-α exponential of e in the field.
+// If e < 0, Exp returns 0.
+func (f *Field) Exp(e int) byte {
+	if e < 0 {
+		return 0
+	}
+	return f.exp[e%f.order]
+}
+
+// Log returns the base-α logarithm of x in the field.
+// If x == 0, Log returns -1.
+func (f *Field) Log(x byte) int {
+	if x == 0 {
+		return -1
+	}
+	return int(f.log[x])
+}
+
+// Inv returns the multiplicative inverse of x in the field.
+// If x == 0, Inv returns 0.
+func (f *Field) Inv(x byte) byte {
+	if x == 0 {
+		return 0
+	}
+	return f.exp[f.order-int(f.log[x])]
+}
+
+// Mul returns the product of x and y in the field.
+func (f *Field) Mul(x, y byte) byte {
+	if x == 0 || y == 0 {
+		return 0
+	}
+	return f.exp[int(f.log[x])+int(f.log[y])]
+}
+
+// Calculate syndrome for a message encoded using the field generated for a
+// particular Reed-Solomon polynomial. Offset defines the coefficient offset.
+func (f *Field) Syndrome(message []byte, paritySymbolCount, offset int) (syndrome []byte) {
+	if offset < 0 || offset > f.order {
+		panic("gf: invalid offset: " + strconv.Itoa(offset))
+	}
+
+	if paritySymbolCount < 0 || paritySymbolCount > len(msg) {
+		panic("gf: invalid paritySymbolCount: " + strconv.Itoa(paritySymbolCount))
+	}
+
+	syndrome = make([]byte, paritySymbolCount)
+
+	for idx, syn := range syndrome {
+		syn = message[0]
+		for _, v := range message[1:] {
+			syn = f.Mul(syn, f.Exp(offset+idx)) ^ v
+		}
+		syndrome[idx] = syn
+	}
+
+	return syndrome
+}
---
+++ r900/r900.go
@@ -0,0 +1,287 @@
+// RTLAMR - An rtl-sdr receiver for smart meters operating in the 900MHz ISM band.
+// Copyright (C) 2014 Douglas Hall
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package r900
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"strconv"
+
+	"github.com/bemasher/rtlamr/r900/gf"
+
+	"github.com/bemasher/rtlamr/decode"
+	"github.com/bemasher/rtlamr/parse"
+)
+
+const (
+	PayloadSymbols = 42
+)
+
+func NewPacketConfig(symbolLength int) (cfg decode.PacketConfig) {
+	cfg.DataRate = 32768
+
+	cfg.CenterFreq = 912380000
+
+	cfg.SymbolLength = symbolLength
+	cfg.SymbolLength2 = cfg.SymbolLength << 1
+
+	cfg.SampleRate = cfg.DataRate * cfg.SymbolLength
+
+	cfg.PreambleSymbols = 32
+	cfg.PacketSymbols = 116
+
+	cfg.PreambleLength = cfg.PreambleSymbols * cfg.SymbolLength2
+	cfg.PacketLength = cfg.PacketSymbols * cfg.SymbolLength2
+
+	cfg.BlockSize = decode.NextPowerOf2(cfg.PreambleLength)
+	cfg.BlockSize2 = cfg.BlockSize << 1
+
+	cfg.BufferLength = cfg.PacketLength + cfg.BlockSize
+
+	cfg.Preamble = "00000000000000001110010101100100"
+
+	return
+}
+
+type Parser struct {
+	decode.Decoder
+	field *gf.Field
+	rsBuf [31]byte
+
+	csum      []float64
+	filtered  [][3]float64
+	quantized []byte
+}
+
+func NewParser(symbolLength int, fastMag bool) (p Parser) {
+	p.Decoder = decode.NewDecoder(NewPacketConfig(symbolLength), fastMag)
+
+	// GF of order 32, polynomial 37, generator 2.
+	p.field = gf.NewField(32, 37, 2)
+
+	p.csum = make([]float64, p.Decoder.Cfg.BufferLength+1)
+	p.filtered = make([][3]float64, p.Decoder.Cfg.BufferLength)
+	p.quantized = make([]byte, p.Decoder.Cfg.BufferLength)
+
+	return
+}
+
+func (p Parser) Dec() decode.Decoder {
+	return p.Decoder
+}
+
+func (p Parser) Cfg() decode.PacketConfig {
+	return p.Decoder.Cfg
+}
+
+// Perform matched filtering.
+func (p Parser) Filter() {
+	// This function computes the convolution of each symbol kernel with the
+	// signal. The naive approach requires for each symbol to calculate the
+	// summation of samples between a pair of indices.
+
+	// 0 |--------|
+	// 1   |--------|
+	// 2     |--------|
+	// 3       |--------|
+
+	// To avoid redundant calculations we compute the cumulative sum of the
+	// signal. This reduces each summation to the difference between the two
+	// indices of the cumulative sum.
+
+	var sum float64
+	for idx, v := range p.Decoder.Signal {
+		sum += v
+		p.csum[idx+1] = sum
+	}
+
+	// There are six symbols, composed of three base symbols and their bitwise
+	// inversions. Compute the convolution of each base symbol with the
+	// signal.
+
+	// 1100 -> 0011
+	// 1010 -> 0101
+	// 1001 -> 0110
+
+	// This is basically unreadable because of a lot of algebraic
+	// simplification but is necessary for efficiency.
+	for idx := 0; idx < p.Decoder.Cfg.BufferLength-p.Decoder.Cfg.SymbolLength*4; idx++ {
+		c0 := p.csum[idx]
+		c1 := p.csum[idx+p.Decoder.Cfg.SymbolLength] * 2
+		c2 := p.csum[idx+p.Decoder.Cfg.SymbolLength*2] * 2
+		c3 := p.csum[idx+p.Decoder.Cfg.SymbolLength*3] * 2
+		c4 := p.csum[idx+p.Decoder.Cfg.SymbolLength*4]
+
+		p.filtered[idx][0] = c2 - c4 - c0           // 1100
+		p.filtered[idx][1] = c1 - c2 + c3 - c4 - c0 // 1010
+		p.filtered[idx][2] = c1 - c3 + c4 - c0      // 1001
+	}
+}
+
+// Determine the symbol that exists at each sample of the signal.
+func (p Parser) Quantize() {
+	// 0 0011, 3 1100
+	// 1 0101, 4 1010
+	// 2 0110, 5 1001
+
+	for idx, vec := range p.filtered {
+		argmax := byte(0)
+		max := math.Abs(vec[0])
+
+		// If v1 is larger than v0, update max and argmax.
+		if v1 := math.Abs(vec[1]); v1 > max {
+			max = v1
+			argmax = 1
+		}
+
+		// If v2 is larger than the greater of v1 or v0, update max and argmax.
+		if v2 := math.Abs(vec[2]); v2 > max {
+			max = v2
+			argmax = 2
+		}
+
+		// Set the output symbol index.
+		p.quantized[idx] = argmax
+
+		// If the sign is negative, jump to the index of the inverted symbol.
+		if vec[argmax] > 0 {
+			p.quantized[idx] += 3
+		}
+	}
+}
+
+// Given a list of indices the preamble exists at, decode and parse a message.
+func (p Parser) Parse(indices []int) (msgs []parse.Message) {
+	p.Filter()
+	p.Quantize()
+
+	preambleLength := p.Decoder.Cfg.PreambleLength
+	symbolLength := p.Decoder.Cfg.SymbolLength
+	zeros := make([]byte, 5)
+
+	seen := make(map[string]bool)
+	for _, preambleIdx := range indices {
+		if preambleIdx > p.Decoder.Cfg.BlockSize {
+			break
+		}
+
+		payloadIdx := preambleIdx + preambleLength
+		var (
+			symbol    string
+			bits      string
+			symbols   [21]byte
+			badSymbol bool
+			symbolIdx int
+		)
+		for idx := 0; idx < PayloadSymbols*4*p.Decoder.Cfg.SymbolLength; idx += symbolLength * 4 {
+			qIdx := payloadIdx + idx
+
+			symbol += strconv.FormatInt(int64(p.quantized[qIdx]), 10)
+			if len(symbol) > 1 {
+				n, _ := strconv.ParseInt(symbol, 6, 64)
+				symbol = ""
+
+				bits += fmt.Sprintf("%05b", n)
+
+				if n > 31 {
+					badSymbol = true
+					break
+				}
+				symbols[symbolIdx] = byte(n)
+				symbolIdx++
+			}
+		}
+
+		if badSymbol || seen[bits] {
+			continue
+		}
+
+		seen[bits] = true
+
+		copy(p.rsBuf[:], symbols[:16])
+		copy(p.rsBuf[26:], symbols[16:])
+		syndromes := p.field.Syndrome(p.rsBuf[:], 5, 29)
+
+		if !bytes.Equal(zeros, syndromes) {
+			continue
+		}
+
+		id, _ := strconv.ParseUint(bits[:32], 2, 32)
+		unkn1, _ := strconv.ParseUint(bits[32:40], 2, 8)
+		unkn2, _ := strconv.ParseUint(bits[40:48], 2, 8)
+		consumption, _ := strconv.ParseUint(bits[48:72], 2, 24)
+		unkn3, _ := strconv.ParseUint(bits[72:74], 2, 2)
+		unkn4, _ := strconv.ParseUint(bits[74:80], 2, 6)
+
+		var r900 R900
+
+		r900.ID = uint32(id)
+		r900.Unkn1 = uint8(unkn1)
+		r900.Unkn2 = uint8(unkn2)
+		r900.Consumption = uint32(consumption)
+		r900.Unkn3 = uint8(unkn3)
+		r900.Unkn4 = uint8(unkn4)
+
+		msgs = append(msgs, r900)
+	}
+
+	return
+}
+
+type R900 struct {
+	ID          uint32 `xml:",attr"` // 32 bits
+	Unkn1       uint8  `xml:",attr"` // 8 bits
+	Unkn2       uint8  `xml:",attr"` // 8 bits
+	Consumption uint32 `xml:",attr"` // 24 bits
+	Unkn3       uint8  `xml:",attr"` // 2 bits
+	Unkn4       uint8  `xml:",attr"` // 6 bits
+}
+
+func (r900 R900) MsgType() string {
+	return "R900"
+}
+
+func (r900 R900) MeterID() uint32 {
+	return r900.ID
+}
+
+func (r900 R900) MeterType() uint8 {
+	return r900.Unkn1
+}
+
+func (r900 R900) String() string {
+	return fmt.Sprintf("{ID:%10d Unkn1:0x%02X Unkn2:0x%02X Consumption:%8d Unkn3:0x%02X Unkn4:0x%02X}",
+		r900.ID,
+		r900.Unkn1,
+		r900.Unkn2,
+		r900.Consumption,
+		r900.Unkn3,
+		r900.Unkn4,
+	)
+}
+
+func (r900 R900) Record() (r []string) {
+	r = append(r, strconv.FormatUint(uint64(r900.ID), 10))
+	r = append(r, strconv.FormatUint(uint64(r900.Unkn1), 10))
+	r = append(r, strconv.FormatUint(uint64(r900.Unkn2), 10))
+	r = append(r, strconv.FormatUint(uint64(r900.Consumption), 10))
+	r = append(r, strconv.FormatUint(uint64(r900.Unkn3), 10))
+	r = append(r, strconv.FormatUint(uint64(r900.Unkn4), 10))
+
+	return
+}
