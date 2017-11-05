bemasher/rtlamr

commit 8a0e86eb6c856f1f71a63b7ebc1f1c1e140f0895

Handle blocks asynchronously.
If we don't handle blocks asynchronously and rtl_tcp isn't sending data
for some reason, this will block forever and rtlamr won't respond
to timeout or ^C.

main.go
@@ -119,10 +119,23 @@ func (rcvr *Receiver) Run() {
 		}
 	}()
 
-	block := make([]byte, rcvr.p.Cfg().BlockSize2)
 	sampleBuf := new(bytes.Buffer)
-
 	start := time.Now()
+
+	blockCh := make(chan []byte)
+	go func() {
+		block := make([]byte, rcvr.p.Cfg().BlockSize2)
+
+		for {
+			// Read new sample block.
+			_, err := io.ReadFull(in, block)
+			if err != nil {
+				log.Fatal("Error reading samples: ", err)
+			}
+			blockCh <- block
+		}
+	}()
+
 	for {
 		// Exit on interrupt or time limit, otherwise receive.
 		select {
@@ -131,13 +144,7 @@ func (rcvr *Receiver) Run() {
 		case <-tLimit:
 			log.Println("Time Limit Reached:", time.Since(start))
 			return
-		default:
-			// Read new sample block.
-			_, err := io.ReadFull(in, block)
-			if err != nil {
-				log.Fatal("Error reading samples: ", err)
-			}
-
+		case block := <-blockCh:
 			// If dumping samples, discard the oldest block from the buffer if
 			// it's full and write the new block to it.
 			if *sampleFilename != os.DevNull {
@@ -161,7 +168,7 @@ func (rcvr *Receiver) Run() {
 				msg.Length = sampleBuf.Len()
 				msg.Message = pkt
 
-				err = encoder.Encode(msg)
+				err := encoder.Encode(msg)
 				if err != nil {
 					log.Fatal("Error encoding message: ", err)
 				}
@@ -183,7 +190,7 @@ func (rcvr *Receiver) Run() {
 
 			if pktFound {
 				if *sampleFilename != os.DevNull {
-					_, err = sampleFile.Write(sampleBuf.Bytes())
+					_, err := sampleFile.Write(sampleBuf.Bytes())
 					if err != nil {
 						log.Fatal("Error writing raw samples to file:", err)
 					}

commit f25fa425743d5bf785690da09a06b23d1bbe4d8c

Fix race condition introduced in 8a0e86e
The block passed to blockCh referenced data that was overwritten on each
iteration of the block generating goroutine. Introduced a sync pool to
reduce allocations and to make sure we don't clobber a block of samples
we've sent for processing.

main.go
@@ -27,6 +27,7 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/bemasher/rtlamr/parse"
@@ -122,17 +123,28 @@ func (rcvr *Receiver) Run() {
 	sampleBuf := new(bytes.Buffer)
 	start := time.Now()
 
-	blockCh := make(chan []byte)
-	go func() {
-		block := make([]byte, rcvr.p.Cfg().BlockSize2)
+	blockCh := make(chan []byte, 128)
+	blockPool := sync.Pool{
+		New: func() interface{} {
+			return make([]byte, rcvr.p.Cfg().BlockSize2)
+		},
+	}
 
+	go func() {
 		for {
+			block := blockPool.Get().([]byte)
+
 			// Read new sample block.
 			_, err := io.ReadFull(in, block)
 			if err != nil {
-				log.Fatal("Error reading samples: ", err)
+				log.Println("Error reading samples: ", err)
+				continue
 			}
-			blockCh <- block
+			select {
+			case blockCh <- block:
+			default:
+				log.Println("Dropped block...")
+			}
 		}
 	}()
 