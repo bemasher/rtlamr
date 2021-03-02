bemasher/rtlamr

commit 60f79de41d8e3519b9aec9ddf7d34ba133d99178

Cleanup synchronization. Fixes #180
rcvr.Run no longer blocks.

main.go
@@ -18,14 +18,16 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-	"net"
 	"os"
 	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/pkg/errors"
@@ -48,15 +50,18 @@ type Receiver struct {
 	d  protocol.Decoder
 	fc protocol.FilterChain
 
-	stop chan struct{}
+	ctx    context.Context
+	cancel context.CancelFunc
+	wg     *sync.WaitGroup
 
 	err error
 }
 
 func (rcvr *Receiver) NewReceiver() {
-	rcvr.d = protocol.NewDecoder()
+	rcvr.ctx, rcvr.cancel = context.WithCancel(context.Background())
+	rcvr.wg = &sync.WaitGroup{}
 
-	rcvr.stop = make(chan struct{}, 1)
+	rcvr.d = protocol.NewDecoder()
 
 	// If the msgtype "all" is given alone, register and use scm, scm+, idm and r900.
 	if _, all := msgType["all"]; all && len(msgType) == 1 {
@@ -122,23 +127,15 @@ func (rcvr *Receiver) NewReceiver() {
 }
 
 func (rcvr *Receiver) Close() {
-	rcvr.stop <- struct{}{}
+	rcvr.cancel()
+	rcvr.wg.Wait()
 	rcvr.SDR.Close()
 }
 
 func (rcvr *Receiver) Run() {
-	// Setup signal channel for interruption.
-	sigint := make(chan os.Signal, 1)
-	signal.Notify(sigint, os.Kill, os.Interrupt)
-
-	// Setup time limit channel
-	tLimit := make(<-chan time.Time, 1)
-	if *timeLimit != 0 {
-		tLimit = time.After(*timeLimit)
-	}
+	rcvr.wg.Add(2)
 
 	sampleBuf := new(bytes.Buffer)
-	start := time.Now()
 
 	// Allocate a channel of blocks.
 	blockCh := make(chan []byte)
@@ -149,18 +146,18 @@ func (rcvr *Receiver) Run() {
 
 	// Read and send sample blocks to the decoder.
 	go func() {
+		defer close(blockCh)
+		defer rcvr.wg.Done()
+
 		// Make two sample blocks, one for reading, and one for the receiver to
 		// decode, these are exchanged each time we read a new block.
 		blockA := make([]byte, rcvr.d.Cfg.BlockSize2)
 		blockB := make([]byte, rcvr.d.Cfg.BlockSize2)
 
-		// When exiting this goroutine, close the block channel.
-		defer close(blockCh)
-
 		for {
 			select {
 			// Exit if we've been told to stop.
-			case <-rcvr.stop:
+			case <-rcvr.ctx.Done():
 				return
 			default:
 				rcvr.err = rcvr.SetDeadline(time.Now().Add(5 * time.Second))
@@ -171,28 +168,9 @@ func (rcvr *Receiver) Run() {
 
 				// Read new sample block.
 				_, rcvr.err = io.ReadFull(rcvr, blockA)
-				rcvr.err = errors.Wrap(rcvr.err, "io.ReadFull")
-
-				// If we get an EOF, exit.
-				if rcvr.err == io.EOF || rcvr.err == io.ErrUnexpectedEOF {
-					return
-				}
-
 				if rcvr.err != nil {
-					switch err := rcvr.err.(type) {
-					// If we get a network operation error.
-					case *net.OpError:
-						if err.Temporary() {
-							// If temporary, keep trying to read.
-							continue
-						} else {
-							// If it's not temporary, exit.
-							return
-						}
-					default:
-						// For everything else, assume it's fatal and bail.
-						return
-					}
+					rcvr.err = errors.Wrap(rcvr.err, "io.ReadFull")
+					return
 				}
 
 				// Send the sample block.
@@ -204,96 +182,97 @@ func (rcvr *Receiver) Run() {
 		}
 	}()
 
-	for {
-		// Exit on interrupt or time limit, otherwise receive.
-		select {
-		case <-sigint:
-			return
-		case <-tLimit:
-			log.Println("Time Limit Reached:", time.Since(start))
-			return
-		case block, ok := <-blockCh:
-			// If blockCh is closed, exit.
-			if !ok {
+	go func() {
+		defer rcvr.wg.Done()
+		for {
+			select {
+			case <-rcvr.ctx.Done():
+				// Consume any in-flight blocks.
+				for range blockCh {
+				}
 				return
-			}
-
-			// Clear next map for this sample block.
-			for key := range next {
-				delete(next, key)
-			}
-
-			// If dumping samples, discard the oldest block from the buffer if
-			// it's full and write the new block to it.
-			if *sampleFilename != os.DevNull {
-				if sampleBuf.Len() > rcvr.d.Cfg.BufferLength<<1 {
-					io.CopyN(ioutil.Discard, sampleBuf, int64(len(block)))
-				}
-				sampleBuf.Write(block)
-			}
-
-			pktFound := false
-
-			// For each message returned
-			for msg := range rcvr.d.Decode(block) {
-				// If the filterchain rejects the message, skip it.
-				if !rcvr.fc.Match(msg) {
+			case block, ok := <-blockCh:
+				if !ok {
 					continue
 				}
 
-				// Make a new LogMessage
-				var logMsg protocol.LogMessage
-				logMsg.Time = time.Now()
-				logMsg.Offset, _ = sampleFile.Seek(0, os.SEEK_CUR)
-				logMsg.Length = sampleBuf.Len()
-				logMsg.Type = msg.MsgType()
-				logMsg.Message = msg
-
-				// This should be unique enough to identify a message between blocks.
-				msgDigest := protocol.NewDigest(msg)
-
-				// Mark the message as seen for the next loop.
-				next[msgDigest] = true
-
-				// If the message was seen in the previous loop, skip it.
-				if prev[msgDigest] {
-					continue
+				// Clear next map for this sample block.
+				for key := range next {
+					delete(next, key)
 				}
 
-				// Encode the message
-				rcvr.err = encoder.Encode(logMsg)
-				rcvr.err = errors.Wrap(rcvr.err, "encoder.Encode")
-
-				if rcvr.err != nil {
-					return
-				}
-
-				pktFound = true
-				if *single {
-					if len(meterID.UintMap) == 0 {
-						break
-					} else {
-						delete(meterID.UintMap, uint(msg.MeterID()))
-					}
-				}
-			}
-
-			if pktFound {
+				// If dumping samples, discard the oldest block from the buffer if
+				// it's full and write the new block to it.
 				if *sampleFilename != os.DevNull {
-					_, err := sampleFile.Write(sampleBuf.Bytes())
-					if err != nil {
-						log.Fatal("Error writing raw samples to file:", err)
+					if sampleBuf.Len() > rcvr.d.Cfg.BufferLength<<1 {
+						io.CopyN(ioutil.Discard, sampleBuf, int64(len(block)))
+					}
+					sampleBuf.Write(block)
+				}
+
+				pktFound := false
+
+				// For each message returned
+				for msg := range rcvr.d.Decode(block) {
+					// If the filterchain rejects the message, skip it.
+					if !rcvr.fc.Match(msg) {
+						continue
+					}
+
+					// Make a new LogMessage
+					var logMsg protocol.LogMessage
+					logMsg.Time = time.Now()
+					logMsg.Offset, _ = sampleFile.Seek(0, os.SEEK_CUR)
+					logMsg.Length = sampleBuf.Len()
+					logMsg.Type = msg.MsgType()
+					logMsg.Message = msg
+
+					// This should be unique enough to identify a message between blocks.
+					msgDigest := protocol.NewDigest(msg)
+
+					// Mark the message as seen for the next loop.
+					next[msgDigest] = true
+
+					// If the message was seen in the previous loop, skip it.
+					if prev[msgDigest] {
+						continue
+					}
+
+					// Encode the message
+					rcvr.err = encoder.Encode(logMsg)
+					rcvr.err = errors.Wrap(rcvr.err, "encoder.Encode")
+
+					if rcvr.err != nil {
+						return
+					}
+
+					pktFound = true
+					if *single {
+						if len(meterID.UintMap) == 0 {
+							break
+						} else {
+							delete(meterID.UintMap, uint(msg.MeterID()))
+						}
 					}
 				}
-				if *single && len(meterID.UintMap) == 0 {
-					return
-				}
-			}
 
-			// Swap next and previous digest maps.
-			next, prev = prev, next
+				if pktFound {
+					if *sampleFilename != os.DevNull {
+						_, err := sampleFile.Write(sampleBuf.Bytes())
+						if err != nil {
+							log.Fatal("Error writing raw samples to file:", err)
+						}
+					}
+					if *single && len(meterID.UintMap) == 0 {
+						return
+					}
+				}
+
+				// Swap next and previous digest maps.
+				next, prev = prev, next
+			}
 		}
-	}
+	}()
 }
 
 func init() {
@@ -333,5 +312,25 @@ func main() {
 		}
 	}()
 
+	start := time.Now()
 	rcvr.Run()
+
+	// Setup signal channel for interruption.
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	// Setup time limit channel
+	timeLimitCh := make(<-chan time.Time, 1)
+	if *timeLimit != 0 {
+		timeLimitCh = time.After(*timeLimit)
+	}
+
+	select {
+	case sig := <-sigCh:
+		log.Println("Received Signal:", sig)
+	case <-timeLimitCh:
+		log.Println("Time Limit Reached:", time.Since(start))
+	}
+
+	rcvr.Close()
 }

go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/bemasher/rtltcp v0.0.0-20151011062038-3aed81c166c5
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898
 )

go.sum
@@ -1,6 +1,13 @@
 github.com/bemasher/rtltcp v0.0.0-20151011062038-3aed81c166c5 h1:QFs9NpZKQY/WPjOfgNN8Qt83scnnhDueQrnjZOG884M=
 github.com/bemasher/rtltcp v0.0.0-20151011062038-3aed81c166c5/go.mod h1:O6JJfPo2Vr2FA+N401mWyEVhwq5Wo/z1dfX+tIKGRUU=
+github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
 github.com/pkg/errors v0.8.1 h1:iURUrRGxPUNPdy5/HRSm+Yj6okJ6UtLINN0Q9M4+h3I=
 github.com/pkg/errors v0.8.1/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
+github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
+github.com/sirupsen/logrus v1.7.0 h1:ShrD1U9pZB12TX0cVy0DtePoCH97K8EtX+mg7ZARUtM=
+github.com/sirupsen/logrus v1.7.0/go.mod h1:yWOB1SBYBC5VeMP7gHvWumXLIWorT60ONWic61uBYv0=
+github.com/stretchr/testify v1.2.2/go.mod h1:a8OnRcib4nhh0OaRAV+Yts87kKdq0PP7pXfy6kDkUVs=
+golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 h1:YyJpGZS1sBuBCzLAR1VEpK193GlqGZbnPFnPV/5Rsb4=
+golang.org/x/sys v0.0.0-20191026070338-33540a1f6037/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 h1:/atklqdjdhuosWIl6AIbOeHJjicWYPqR9bpxqxYG2pA=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

commit 2335efc36f55fdc0733de73049d494654f7f1956

Fixes what 60f79de broke.

main.go
@@ -133,7 +133,7 @@ func (rcvr *Receiver) Close() {
 }
 
 func (rcvr *Receiver) Run() {
-	rcvr.wg.Add(2)
+	rcvr.wg.Add(3)
 
 	sampleBuf := new(bytes.Buffer)
 
@@ -144,8 +144,17 @@ func (rcvr *Receiver) Run() {
 	prev := map[protocol.Digest]bool{}
 	next := map[protocol.Digest]bool{}
 
+	go func() {
+		defer rcvr.wg.Done()
+		<-rcvr.ctx.Done()
+		// Consume any in-flight blocks.
+		for range blockCh {
+		}
+	}()
+
 	// Read and send sample blocks to the decoder.
 	go func() {
+		defer rcvr.cancel()
 		defer close(blockCh)
 		defer rcvr.wg.Done()
 
@@ -183,13 +192,12 @@ func (rcvr *Receiver) Run() {
 	}()
 
 	go func() {
+		defer rcvr.cancel()
 		defer rcvr.wg.Done()
+
 		for {
 			select {
 			case <-rcvr.ctx.Done():
-				// Consume any in-flight blocks.
-				for range blockCh {
-				}
 				return
 			case block, ok := <-blockCh:
 				if !ok {
@@ -264,6 +272,7 @@ func (rcvr *Receiver) Run() {
 						}
 					}
 					if *single && len(meterID.UintMap) == 0 {
+						rcvr.cancel()
 						return
 					}
 				}
@@ -330,6 +339,8 @@ func main() {
 		log.Println("Received Signal:", sig)
 	case <-timeLimitCh:
 		log.Println("Time Limit Reached:", time.Since(start))
+	case <-rcvr.ctx.Done():
+		log.Println("Receiver context cancelled.")
 	}
 
 	rcvr.Close()